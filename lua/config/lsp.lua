68198a2bb9b863651978ad63f958324bf19767cc
Ricardo Antolin <ricardo.antolin@cabify.com>
2025-04-12 20:21:56 +0200

    post merge fixes

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -25,15 +25,12 @@ local custom_attach = function(client, bufnr)
 
   map("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
   map("n", "gi", vim.lsp.buf.implementation, { desc = "go to implementation" })
-  map("n", "K", vim.lsp.buf.hover)
+    map("n", "K", function()
+      vim.lsp.buf.hover { border = "single", max_height = 25, max_width = 120 }
+    end)
   map("n", "<C-k>", vim.lsp.buf.signature_help)
   map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
   map("n", "gr", vim.lsp.buf.references, { desc = "show references" })
-  map("n", "[d", diagnostic.get_prev, { desc = "previous diagnostic" })
-  map("n", "]d", diagnostic.get_next, { desc = "next diagnostic" })
-  -- this puts diagnostics from opened files to quickfix
-  map("n", "<space>qw", diagnostic.setqflist, { desc = "put window diagnostics to qf" })
-  -- this puts diagnostics from current buffer to quickfix
   map("n", "<space>qb", function()
     set_qflist(bufnr)
   end, { desc = "put buffer diagnostics to qf" })
@@ -191,85 +188,18 @@ if utils.executable("ruff") then
   }
 end
 
--- Disable ruff hover feature in favor of Pyright
->>>>>>> 8f2b3a7 (deprecation fixes, adds ruby-lsp, treesitter and codeium, deletes)
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp_buf_conf", { clear = true }),
-  callback = function(event_context)
-    local client = vim.lsp.get_client_by_id(event_context.data.client_id)
-    -- vim.print(client.name, client.server_capabilities)
-
-    if not client then
-      return
-    end
-
-    local bufnr = event_context.buf
-
-    -- Mappings.
-    local map = function(mode, l, r, opts)
-      opts = opts or {}
-      opts.silent = true
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
-
-    map("n", "gd", function()
-      vim.lsp.buf.definition {
-        on_list = function(options)
-          -- custom logic to avoid showing multiple definition when you use this style of code:
-          -- `local M.my_fn_name = function() ... end`.
-          -- See also post here: https://www.reddit.com/r/neovim/comments/19cvgtp/any_way_to_remove_redundant_definition_in_lua_file/
-
-          -- vim.print(options.items)
-          local unique_defs = {}
-          local def_loc_hash = {}
-
-          -- each item in options.items contain the location info for a definition provided by LSP server
-          for _, def_location in pairs(options.items) do
-            -- use filename and line number to uniquelly indentify a definition,
-            -- we do not expect/want multiple definition in single line!
-            local hash_key = def_location.filename .. def_location.lnum
-
-            if not def_loc_hash[hash_key] then
-              def_loc_hash[hash_key] = true
-              table.insert(unique_defs, def_location)
-            end
-          end
-
-          options.items = unique_defs
-
-          -- set the location list
-          ---@diagnostic disable-next-line: param-type-mismatch
-          vim.fn.setloclist(0, {}, " ", options)
-
-          -- open the location list when we have more than 1 definitions found,
-          -- otherwise, jump directly to the definition
-          if #options.items > 1 then
-            vim.cmd.lopen()
-          else
-            vim.cmd([[silent! lfirst]])
-          end
-        end,
-      }
-    end, { desc = "go to definition" })
-    map("n", "<C-]>", vim.lsp.buf.definition)
-    map("n", "K", function()
-      vim.lsp.buf.hover { border = "single", max_height = 25, max_width = 120 }
-    end)
-    map("n", "<C-k>", vim.lsp.buf.signature_help)
-    map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
-    map("n", "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
-    map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
-    map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
-    map("n", "<space>wl", function()
-      vim.print(vim.lsp.buf.list_workspace_folders())
-    end, { desc = "list workspace folder" })
-
-    -- Set some key bindings conditional on server capabilities
-    -- Disable ruff hover feature in favor of Pyright
-    if client.name == "ruff" then
-      client.server_capabilities.hoverProvider = false
-    end
+if utils.executable("ltex-ls") then
+  lspconfig.ltex.setup {
+    cmd = { "ltex-ls" },
+    filetypes = { "text", "plaintex", "tex", "markdown" },
+    settings = {
+      ltex = {
+        language = "en",
+      },
+    },
+    flags = { debounce_text_changes = 300 },
+  }
+end
 
     -- Uncomment code below to enable inlay hint from language server, some LSP server supports inlay hint,
     -- but disable this feature by default, so you may need to enable inlay hint in the LSP server config.
